0a577593d72f8e77ae7cf511db37419c82750b88
Anirudh Sridhar <anirudh@blogvault.net>
2018-10-15 09:45:33 +0530

    #5 adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	"extends": ["eslint:recommended", "plugin:react/recommended"],
+	"rules": {
+		"indent": ["error", "tab"],
+		"no-tabs": ["off"],
+		"prefer-template": ["error"],
+		"react/jsx-uses-vars": ["error"],
+		"no-var": ["error"],
+		"semi": ["error", "never"],
+		"no-console": ["warn"],
+		"prefer-arrow-callback": ["error"],
+		"eqeqeq": ["warn", "always"],
+		"keyword-spacing": ["error"],
+		"brace-style": ["error", "1tbs"],
+		"no-unused-vars": ["warn"],
+		"react/prop-types": ["warn"]
+	},
+	"parser": "babel-eslint",
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true,
+			"modules": true
+		}
+	},
+	"env": {
+		"browser": true,
+		"node": true,
+		"jasmine": true
+	},
+};
